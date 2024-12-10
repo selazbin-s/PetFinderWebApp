89a336f75f12be8e7a0a5953a17ac6fbde63cfee
selazbin-s <79288748+selazbin-s@users.noreply.github.com>
2024-12-10 01:55:19 -0800

    Update pet_traits.pl

--- Prolog/pet_traits.pl
+++ Prolog/pet_traits.pl
@@ -1,101 +1,200 @@
-% Facts about cats
-pet(cat1, calm, no_shed, apartment, small).
-pet(cat2, calm, no_shed, apartment, large).
-pet(cat3, calm, no_shed, house, small).
-pet(cat4, calm, no_shed, house, large).
-pet(cat5, calm, shed, apartment, small).
-pet(cat6, calm, shed, apartment, large).
-pet(cat7, calm, shed, house, small).
-pet(cat8, calm, shed, house, large).
+pet(cat1, calm, no_shed, small, baby, trained).
+pet(cat2, calm, no_shed, small, baby, no_trained).
+pet(cat3, calm, no_shed, small, youth, trained).
+pet(cat4, calm, no_shed, small, youth, no_trained).
+pet(cat5, calm, no_shed, small, senior, trained).
+pet(cat6, calm, no_shed, small, senior, no_trained).
+pet(cat7, calm, no_shed, large, baby, trained).
+pet(cat8, calm, no_shed, large, baby, no_trained).
+pet(cat9, calm, no_shed, large, youth, trained).
+pet(cat10, calm, no_shed, large, youth, no_trained).
+pet(cat11, calm, no_shed, large, senior, trained).
+pet(cat12, calm, no_shed, large, senior, no_trained).
+pet(cat13, calm, shed, small, baby, trained).
+pet(cat14, calm, shed, small, baby, no_trained).
+pet(cat15, calm, shed, small, youth, trained).
+pet(cat16, calm, shed, small, youth, no_trained).
+pet(cat17, calm, shed, small, senior, trained).
+pet(cat18, calm, shed, small, senior, no_trained).
+pet(cat19, calm, shed, large, baby, trained).
+pet(cat20, calm, shed, large, baby, no_trained).
+pet(cat21, calm, shed, large, youth, trained).
+pet(cat22, calm, shed, large, youth, no_trained).
+pet(cat23, calm, shed, large, senior, trained).
+pet(cat24, calm, shed, large, senior, no_trained).
+pet(cat25, neutral, no_shed, small, baby, trained).
+pet(cat26, neutral, no_shed, small, baby, no_trained).
+pet(cat27, neutral, no_shed, small, youth, trained).
+pet(cat28, neutral, no_shed, small, youth, no_trained).
+pet(cat29, neutral, no_shed, small, senior, trained).
+pet(cat30, neutral, no_shed, small, senior, no_trained).
+pet(cat31, neutral, no_shed, large, baby, trained).
+pet(cat32, neutral, no_shed, large, baby, no_trained).
+pet(cat33, neutral, no_shed, large, youth, trained).
+pet(cat34, neutral, no_shed, large, youth, no_trained).
+pet(cat35, neutral, no_shed, large, senior, trained).
+pet(cat36, neutral, no_shed, large, senior, no_trained).
+pet(cat37, neutral, shed, small, baby, trained).
+pet(cat38, neutral, shed, small, baby, no_trained).
+pet(cat39, neutral, shed, small, youth, trained).
+pet(cat40, neutral, shed, small, youth, no_trained).
+pet(cat41, neutral, shed, small, senior, trained).
+pet(cat42, neutral, shed, small, senior, no_trained).
+pet(cat43, neutral, shed, large, baby, trained).
+pet(cat44, neutral, shed, large, baby, no_trained).
+pet(cat45, neutral, shed, large, youth, trained).
+pet(cat46, neutral, shed, large, youth, no_trained).
+pet(cat47, neutral, shed, large, senior, trained).
+pet(cat48, neutral, shed, large, senior, no_trained).
+pet(cat49, high_energy, no_shed, small, baby, trained).
+pet(cat50, high_energy, no_shed, small, baby, no_trained).
+pet(cat51, high_energy, no_shed, small, youth, trained).
+pet(cat52, high_energy, no_shed, small, youth, no_trained).
+pet(cat53, high_energy, no_shed, small, senior, trained).
+pet(cat54, high_energy, no_shed, small, senior, no_trained).
+pet(cat55, high_energy, no_shed, large, baby, trained).
+pet(cat56, high_energy, no_shed, large, baby, no_trained).
+pet(cat57, high_energy, no_shed, large, youth, trained).
+pet(cat58, high_energy, no_shed, large, youth, no_trained).
+pet(cat59, high_energy, no_shed, large, senior, trained).
+pet(cat60, high_energy, no_shed, large, senior, no_trained).
+pet(cat61, high_energy, shed, small, baby, trained).
+pet(cat62, high_energy, shed, small, baby, no_trained).
+pet(cat63, high_energy, shed, small, youth, trained).
+pet(cat64, high_energy, shed, small, youth, no_trained).
+pet(cat65, high_energy, shed, small, senior, trained).
+pet(cat66, high_energy, shed, small, senior, no_trained).
+pet(cat67, high_energy, shed, large, baby, trained).
+pet(cat68, high_energy, shed, large, baby, no_trained).
+pet(cat69, high_energy, shed, large, youth, trained).
+pet(cat70, high_energy, shed, large, youth, no_trained).
+pet(cat71, high_energy, shed, large, senior, trained).
+pet(cat72, high_energy, shed, large, senior, no_trained).
 
-pet(cat9, neutral, no_shed, apartment, small).
-pet(cat10, neutral, no_shed, apartment, large).
-pet(cat11, neutral, no_shed, house, small).
-pet(cat12, neutral, no_shed, house, large).
-pet(cat13, neutral, shed, apartment, small).
-pet(cat14, neutral, shed, apartment, large).
-pet(cat15, neutral, shed, house, small).
-pet(cat16, neutral, shed, house, large).
+pet(dog1, calm, no_shed, small, baby, trained).
+pet(dog2, calm, no_shed, small, baby, no_trained).
+pet(dog3, calm, no_shed, small, youth, trained).
+pet(dog4, calm, no_shed, small, youth, no_trained).
+pet(dog5, calm, no_shed, small, senior, trained).
+pet(dog6, calm, no_shed, small, senior, no_trained).
+pet(dog7, calm, no_shed, large, baby, trained).
+pet(dog8, calm, no_shed, large, baby, no_trained).
+pet(dog9, calm, no_shed, large, youth, trained).
+pet(dog10, calm, no_shed, large, youth, no_trained).
+pet(dog11, calm, no_shed, large, senior, trained).
+pet(dog12, calm, no_shed, large, senior, no_trained).
+pet(dog13, calm, shed, small, baby, trained).
+pet(dog14, calm, shed, small, baby, no_trained).
+pet(dog15, calm, shed, small, youth, trained).
+pet(dog16, calm, shed, small, youth, no_trained).
+pet(dog17, calm, shed, small, senior, trained).
+pet(dog18, calm, shed, small, senior, no_trained).
+pet(dog19, calm, shed, large, baby, trained).
+pet(dog20, calm, shed, large, baby, no_trained).
+pet(dog21, calm, shed, large, youth, trained).
+pet(dog22, calm, shed, large, youth, no_trained).
+pet(dog23, calm, shed, large, senior, trained).
+pet(dog24, calm, shed, large, senior, no_trained).
+pet(dog25, neutral, no_shed, small, baby, trained).
+pet(dog26, neutral, no_shed, small, baby, no_trained).
+pet(dog27, neutral, no_shed, small, youth, trained).
+pet(dog28, neutral, no_shed, small, youth, no_trained).
+pet(dog29, neutral, no_shed, small, senior, trained).
+pet(dog30, neutral, no_shed, small, senior, no_trained).
+pet(dog31, neutral, no_shed, large, baby, trained).
+pet(dog32, neutral, no_shed, large, baby, no_trained).
+pet(dog33, neutral, no_shed, large, youth, trained).
+pet(dog34, neutral, no_shed, large, youth, no_trained).
+pet(dog35, neutral, no_shed, large, senior, trained).
+pet(dog36, neutral, no_shed, large, senior, no_trained).
+pet(dog37, neutral, shed, small, baby, trained).
+pet(dog38, neutral, shed, small, baby, no_trained).
+pet(dog39, neutral, shed, small, youth, trained).
+pet(dog40, neutral, shed, small, youth, no_trained).
+pet(dog41, neutral, shed, small, senior, trained).
+pet(dog42, neutral, shed, small, senior, no_trained).
+pet(dog43, neutral, shed, large, baby, trained).
+pet(dog44, neutral, shed, large, baby, no_trained).
+pet(dog45, neutral, shed, large, youth, trained).
+pet(dog46, neutral, shed, large, youth, no_trained).
+pet(dog47, neutral, shed, large, senior, trained).
+pet(dog48, neutral, shed, large, senior, no_trained).
+pet(dog49, high_energy, no_shed, small, baby, trained).
+pet(dog50, high_energy, no_shed, small, baby, no_trained).
+pet(dog51, high_energy, no_shed, small, youth, trained).
+pet(dog52, high_energy, no_shed, small, youth, no_trained).
+pet(dog53, high_energy, no_shed, small, senior, trained).
+pet(dog54, high_energy, no_shed, small, senior, no_trained).
+pet(dog55, high_energy, no_shed, large, baby, trained).
+pet(dog56, high_energy, no_shed, large, baby, no_trained).
+pet(dog57, high_energy, no_shed, large, youth, trained).
+pet(dog58, high_energy, no_shed, large, youth, no_trained).
+pet(dog59, high_energy, no_shed, large, senior, trained).
+pet(dog60, high_energy, no_shed, large, senior, no_trained).
+pet(dog61, high_energy, shed, small, baby, trained).
+pet(dog62, high_energy, shed, small, baby, no_trained).
+pet(dog63, high_energy, shed, small, youth, trained).
+pet(dog64, high_energy, shed, small, youth, no_trained).
+pet(dog65, high_energy, shed, small, senior, trained).
+pet(dog66, high_energy, shed, small, senior, no_trained).
+pet(dog67, high_energy, shed, large, baby, trained).
+pet(dog68, high_energy, shed, large, baby, no_trained).
+pet(dog69, high_energy, shed, large, youth, trained).
+pet(dog70, high_energy, shed, large, youth, no_trained).
+pet(dog71, high_energy, shed, large, senior, trained).
+pet(dog72, high_energy, shed, large, senior, no_trained).
 
-pet(cat17, high_energy, no_shed, apartment, small).
-pet(cat18, high_energy, no_shed, apartment, large).
-pet(cat19, high_energy, no_shed, house, small).
-pet(cat20, high_energy, no_shed, house, large).
-pet(cat21, high_energy, shed, apartment, small).
-pet(cat22, high_energy, shed, apartment, large).
-pet(cat23, high_energy, shed, house, small).
-pet(cat24, high_energy, shed, house, large).
+% House Size Check
+house_size(Pet, apartment) :- pet(Pet, _, _, small, _, _).  % Only small pets for apartments
+house_size(Pet, house) :- pet(Pet, _, _, _, _, _).          % All pets for houses
 
-% Facts about dogs
-pet(dog1, calm, no_shed, apartment, small).
-pet(dog2, calm, no_shed, apartment, large).
-pet(dog3, calm, no_shed, house, small).
-pet(dog4, calm, no_shed, house, large).
-pet(dog5, calm, shed, apartment, small).
-pet(dog6, calm, shed, apartment, large).
-pet(dog7, calm, shed, house, small).
-pet(dog8, calm, shed, house, large).
+% Family-friendliness
+family_friendly_pet(_, no_children).  % All pets are suitable for families without children
+family_friendly_pet(Pet, small_children) :-
+    \+ (pet(Pet, high_energy, _, large, _, no_trained)).  % Exclude large, high-energy, untrained pets
 
-pet(dog9, neutral, no_shed, apartment, small).
-pet(dog10, neutral, no_shed, apartment, large).
-pet(dog11, neutral, no_shed, house, small).
-pet(dog12, neutral, no_shed, house, large).
-pet(dog13, neutral, shed, apartment, small).
-pet(dog14, neutral, shed, apartment, large).
-pet(dog15, neutral, shed, house, small).
-pet(dog16, neutral, shed, house, large).
+% Check allergy constraints
+allergy_check(allergies, Pet) :- pet(Pet, _, no_shed, _, _, _).  % Allow only allergy-friendly pets
+allergy_check(no_allergies, _).  % Allow all pets if no allergies
 
-pet(dog17, high_energy, no_shed, apartment, small).
-pet(dog18, high_energy, no_shed, apartment, large).
-pet(dog19, high_energy, no_shed, house, small).
-pet(dog20, high_energy, no_shed, house, large).
-pet(dog21, high_energy, shed, apartment, small).
-pet(dog22, high_energy, shed, apartment, large).
-pet(dog23, high_energy, shed, house, small).
-pet(dog24, high_energy, shed, house, large).
+% Check preference for animal
+preference_animal(_, no_preference).
+preference_animal(Pet, dog_preference) :- sub_atom(Pet, 0, _, _, dog).
+preference_animal(Pet, cat_preference) :- sub_atom(Pet, 0, _, _, cat).
 
-% Derived rules
-small_pet(Pet) :- pet(Pet, _, _, _, small).
-large_pet(Pet) :- pet(Pet, _, _, _, large).
-low_energy_pet(Pet) :- pet(Pet, calm, _, _, _).
-high_energy_pet(Pet) :- pet(Pet, high_energy, _, _, _).
-allergy_friendly_pet(Pet) :- pet(Pet, _, no_shed, _, _).
+% If no yard then no high-energy dogs
+yard_check(_, yes).  % If the user has a yard, all pets are allowed
+yard_check(Pet, no) :-
+    \+ (pet(Pet, high_energy, _, _, _, no_trained), sub_atom(Pet, 0, _, _, dog)).  % Exclude untrained, high-energy dogs if no yard
 
-% Rule: Ensure allergy-friendly pets are labeled as no_shed
-allergy_friendly_pet(Pet) :- pet(Pet, _, no_shed, _, _).
+% Check preference for age
+preference_age(_, no_preference).  % No preference for age
+preference_age(Pet, Age) :- pet(Pet, _, _, _, Age, _).  % Match specific age preference
 
-% Rule: Disallow large pets in apartments
-valid_pet(Pet) :-
-    pet(Pet, _, _, apartment, small).  % Allow small pets in apartments
-valid_pet(Pet) :-
-    pet(Pet, _, _, house, _).         % Allow all pets in houses
+% No two babies at the same time
+children_baby(_, no_children).  % No restriction if there are no children
+children_baby(Pet, small_children) :- 
+    \+ (pet(Pet, _, _, _, baby, no_trained)).  % Exclude baby animals if they are untrained and there are small children
 
-% Rule: Disallow large high-energy pets for families with small children
-family_friendly_pet(Pet, no_children) :- 
-    valid_pet(Pet).  % If no small children, defer to valid_pet/1
-family_friendly_pet(Pet, small_children) :- 
-    valid_pet(Pet),  % Must be valid first
-    \+ (pet(Pet, high_energy, _, _, large)).  % Exclude large high-energy pets
+% Beginner pet
+beginner_pet(Pet, beginner) :- 
+    (pet(Pet, _, _, small, _, trained), sub_atom(Pet, 0, _, _, cat));  % Small, trained cats
+    (pet(Pet, _, _, _, _, trained), sub_atom(Pet, 0, _, _, dog)).     % Any size, trained dogs
+beginner_pet(_, experienced).  % No restriction for experienced pet owners
 
-% Rule: Ensure pets are valid and allergy-friendly if user has allergies
-user_pet(Pet, no_allergies, no_children, no_preference) :-
-    valid_pet(Pet), family_friendly_pet(Pet, no_children).
-user_pet(Pet, no_allergies, small_children, no_preference) :-
-    valid_pet(Pet), family_friendly_pet(Pet, small_children).
-user_pet(Pet, allergies, no_children, no_preference) :-
-    valid_pet(Pet), family_friendly_pet(Pet, no_children), allergy_friendly_pet(Pet).
-user_pet(Pet, allergies, small_children, no_preference) :-
-    valid_pet(Pet), family_friendly_pet(Pet, small_children), allergy_friendly_pet(Pet).
+% Rule to ensure only trained pets are selected if the user does not want to train an animal
+training_preference(_, willing).  % If the user is willing to train, no restriction
+training_preference(Pet, not_willing) :-
+    \+ pet(Pet, _, _, _, _, no_trained).  % Exclude untrained pets if the user does not want to train
 
-% Rule: Handle user preferences for dogs, cats, or no preference
-user_pet(Pet, Allergies, Children, dog_preference) :-
-    valid_pet(Pet), family_friendly_pet(Pet, Children), pet(Pet, _, _, _, _), sub_atom(Pet, 0, _, _, dog), allergy_friendly_check(Allergies, Pet).
-user_pet(Pet, Allergies, Children, cat_preference) :-
-    valid_pet(Pet), family_friendly_pet(Pet, Children), pet(Pet, _, _, _, _), sub_atom(Pet, 0, _, _, cat), allergy_friendly_check(Allergies, Pet).
-user_pet(Pet, Allergies, Children, no_preference) :-
-    user_pet(Pet, Allergies, Children, dog_preference); user_pet(Pet, Allergies, Children, cat_preference).
-
-% Rule: Helper to check allergy constraints
-allergy_friendly_check(allergies, Pet) :-
-    allergy_friendly_pet(Pet).
-allergy_friendly_check(no_allergies, _).
+% User preferences
+user_pet(Pet, Allergies, Children, Preference, Living, Yard, Age, Experience, Training) :-
+    house_size(Pet, Living),  % Match the pets size to the living environment
+    family_friendly_pet(Pet, Children),  % Ensure family-friendliness
+    children_baby(Pet, Children),  % Ensure age is safe for children
+    allergy_check(Allergies, Pet),  % Check allergy constraints
+    preference_animal(Pet, Preference),  % Match species preference
+    yard_check(Pet, Yard),  % Ensure suitability based on yard availability
+    preference_age(Pet, Age),  % Match the pets age preference
+    beginner_pet(Pet, Experience),  % Match beginner pet owner preferences
+    training_preference(Pet, Training).  % Ensure the pet matches the users training willingness
